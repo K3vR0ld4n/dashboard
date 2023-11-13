1ee9b42944f9d571c1c78aa6b6083b394927461c
K3vR0ld4n <107887773+K3vR0ld4n@users.noreply.github.com>
2023-11-13 15:01:06 +0000

    cors proxy

--- public/javascript/load_data.js
+++ public/javascript/load_data.js
@@ -202,7 +202,6 @@ let selectListener = async (event) => {
       await parseXML(responseText);
 
       // Guarde la entrada de almacenamiento local
-
     } catch (error) {
       console.log(error);
     }
@@ -218,10 +217,31 @@ let loadForecastByCity = () => {
   selectElement.addEventListener("change", selectListener);
 };
 
+let verifyLocalTime = () => {};
 
-let verifyLocalTime=()=>{
-  
-}
+//Clase 4
 
+let loadExternalTable = async () => {
+  //Requerimiento asíncrono
+  let url = "https://www.gestionderiesgos.gob.ec/monitoreo-de-inundaciones/";
 
+  //cors proxy
+  let proxyURL = 'https://cors-anywhere.herokuapp.com/'
+  let endpoint = proxyURL + url
+
+  let response = await fetch(endpoint);
+  let responseText = await response.text();
+
+  const parser = new DOMParser();
+  const xml = parser.parseFromString(responseText, "text/html");
+
+  console.log("que mirah bobo" + xml);
+
+  let elementoXML = xml.querySelector("#postcontent table");
+  let elementoDOM = document.getElementById("monitoreo");
+
+  elementoDOM.innerHTML = elementoXML.outerHTML;
+};
+
+loadExternalTable();
 loadForecastByCity();
